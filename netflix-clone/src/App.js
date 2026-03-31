daf3bbb04a9dc76494616d3dbde776f180eeb27a
10ra <jaymarcgososo@gmail.com>
2026-03-31 15:17:03 +0800

    Add: row.css, fetch blocks on App.js. Fix: requests.js

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,21 @@
 
 import './App.css';
+import Row from './Row';
+import React from 'react';
+import requests from './Requests';
 
 function App() {
   return (
     <div className="App">
-      <h1>123123 </h1> 
+      <h1>12312 1231233 </h1> 
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>   
+
     </div>
   );
 }
